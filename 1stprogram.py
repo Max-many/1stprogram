76a71768db5d69c203daa03e6274d833da281150
Max-many <maxlopezepifanov@gmail.com>
2024-10-12 13:32:51 +0300

    Update 1stprogram.py

--- 1stprogram.py
+++ 1stprogram.py
@@ -1,1 +1,2 @@
-print(9**0.5*5)
+C:\Users\user\PycharmProjects\pythonProject9\.venv\Scripts\python.exe C:\Users\user\PycharmProjects\pythonProject9\1stprogram.py 
+C:\Users\user\PycharmProjects\pythonProject9\.venv\Scripts\python.exe C:\Users\user\PycharmProjects\pythonProject9\1stprogram.py 
